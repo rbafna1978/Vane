d36372247914f192b425c306d98fa37d3790ead2
Rishit Bafna <bafnarishit@gmail.com>
2026-09-03 12:03:33 -0700

    Initial Commit

---
+++ WeatherMan/WeatherManApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherManApp.swift
+//  WeatherMan
+//
+//  Created by Rishit Bafna on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
